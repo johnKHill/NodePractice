1db82cca4584b0cfda5ee051ab574362acc51b51
Johknotter Hill <johknotterhill@gmail.com>
2018-06-27 19:13:58 -0400

    Updated the views

--- lbj/routes/index.js
+++ lbj/routes/index.js
@@ -6,24 +6,25 @@ router.get('/', function(req, res, next) {
 //res.render takes 2 args:
 // 1.The name of the view to load
 // 2.An object to send to that view
-	const atlTeams = [
-	`Braves`,
-	`Falcons`, 
-	`United`, 
-	`Hawks`
-	]
+    const atlTeams = [
+    `Braves`,
+    `Falcons`, 
+    `United`, 
+    `Hawks`
+    ]
   res.render('index', { atlTeams: atlTeams });
+ // res.render('index', { atlTeams }); //This is ES6
 });
 
 router.get(`/football`, (req, res)=> {
-	const atlTeams = [
-	`The Player City`,
-	`Braves`,
-	`Falcons`, 
-	`United`, 
-	`Hawks`
-	]
-	res.render(`index`, {atlTeams});
+    const atlTeams = [
+    `The Player City`,
+    `Braves`,
+    `Falcons`, 
+    `United`, 
+    `Hawks`
+    ]
+    res.render(`index`, {atlTeams});
 });
 
 module.exports = router;
