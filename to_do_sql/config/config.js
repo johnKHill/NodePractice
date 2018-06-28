06f0c2aee59d7381f15d4ed1e56955d9518cfd88
Johknotter Hill <johknotterhill@gmail.com>
2018-06-28 19:57:44 -0400

    Added mySql database

--- to_do_sql/config/config.js
+++ to_do_sql/config/config.js
@@ -1,7 +1,7 @@
 const db = {
 	host: `127.0.0.1`,
-	user: `todo`,
-	password: `todo`,
+	user: `root`,
+	password: `root`,
 	database: `to_do`
 }
 module.exports = db
--- to_do_sql/routes/index.js
+++ to_do_sql/routes/index.js
@@ -12,69 +12,69 @@ connection.connect();
 //AFTER ALL THAT PAIN, WE CAN START WRITING AWESOME QUERY
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	let message = req.query.msg;
-	if(message === `added`){
-		message = `Your task was added!`;
-	}else if(message === `deleted`){
-		message = `Your taks was deleted!`;
-	}
+    let message = req.query.msg;
+    if (message === `added`) {
+        message = `Your task was added!`;
+    } else if (message === `deleted`) {
+        message = `Your tasks was deleted!`;
+    }
 
-const selectQuery = `SELECT * FROM tasks`;
+    const selectQuery = `SELECT * FROM tasks`;
 
-connection.query(selectQuery,(error, results)=>{
-		if(error){throw error;}
-		res.render(`index`, {
-			message,
-			title: `Express`,
-			taskArray: results
-		});
-	})	
+    connection.query(selectQuery, (error, results) => {
+        if (error) { throw error; }
+        res.render(`index`, {
+            message,
+            title: `Express`,
+            taskArray: results
+        });
+    })
 });
 
-router.post(`/addItem`,(req, res)=>{
-	const taskName = req.body.newTask;
-	const taskDate = req.body.newTaskDate;
-	// res...
-	// 1. send
-	// 2. render
-	// 3. json
-	// req.query is for get requests or query string
-	// req.body is for data passed in via post
-	// we know that someone submitted the form. 
-	// We know that because they are at the post, addItem route.
-	// we got the data the form sent out of the req.object and stored it. 
-	// Now we need to put it into our Database (mysql)
+router.post(`/addItem`, (req, res) => {
+    const taskName = req.body.newTask;
+    const taskDate = req.body.newTaskDate;
+    // res...
+    // 1. send
+    // 2. render
+    // 3. json
+    // req.query is for get requests or query string
+    // req.body is for data passed in via post
+    // we know that someone submitted the form. 
+    // We know that because they are at the post, addItem route.
+    // we got the data the form sent out of the req.object and stored it. 
+    // Now we need to put it into our Database (mysql)
 
-	
-	const insertQuery = 
-	`INSERT into tasks
-		(task_name, task_date)
-		VALUES
-		(?,?);`
 
-	// query args:
-	// 1. Query sting
-	// 2. array correspondingt to ? in query
-	// 3. callback to run when query is finished	
-	console.log(`$$$$$$$$$$$$$$$$$$$$READY TO RUN THE QUERY$$$$$$$$$$$$$`);
-	
-	connection.query(insertQuery,[taskName, taskDate],(error, results)=>{
-		if(error)throw error;
-		console.log(`=============QUERY DID NOT ERROR< SENDING TO THE HOMEPAGE`);
-		res.redirect(`/?msg=added`);
-	})
+    const insertQuery =
+        `INSERT into tasks
+        (task_name, task_date)
+        VALUES
+        (?,?);`;
 
-	//res.json(req.query);
+    // query args:
+    // 1. Query sting
+    // 2. array corresponding to ? in query
+    // 3. callback to run when query is finished    
+    console.log(`$$$$$$$$$$$$$$$$$$$$ READY TO RUN THE QUERY $$$$$$$$$$$$$`);
+
+    connection.query(insertQuery, [taskName, taskDate], (error, results) => {
+        if (error) throw error;
+        console.log(`============= QUERY DID NOT ERROR< SENDING TO THE HOMEPAGE ===========`);
+        res.redirect(`/?msg=added`);
+    })
+
+    //res.json(req.query);
 
 });
 
-router.get(`/delete/:id`,(req, res)=>{
-	const idToDelete = req.params.id;
-	const deleteQuery = `DELETE FROM tasks WHERE id =?`;
-	connection.query(deleteQuery,[idToDelete],(error,results)=>{
-		if(error){throw error;}
-		res.redirect(`/?msg=deleted`);
-	})
+router.get(`/delete/:id`, (req, res) => {
+    const idToDelete = req.params.id;
+    const deleteQuery = `DELETE FROM tasks WHERE id =?`;
+    connection.query(deleteQuery, [idToDelete], (error, results) => {
+        if (error) { throw error; }
+        res.redirect(`/?msg=deleted`);
+    })
 })
 
 module.exports = router;
