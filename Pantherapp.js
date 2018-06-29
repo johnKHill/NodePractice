c8d1e5cf96807c442f8ebbe628b6356774414483
Johknotter Hill <johknotterhill@gmail.com>
2018-06-28 20:26:24 -0400

    Update panther

--- Pantherapp.js
+++ Pantherapp.js
@@ -28,6 +28,6 @@ const server = http.createServer((req, res)=>{
 	}	
 })
 
-server.listen(4000);
-console.log(`Server is listening on port 4000...`);
+server.listen(3000);
+console.log(`Server is listening on port 3000...`);
 
